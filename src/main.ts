41435622ad5479caf112b8991d842be0dcbfc503
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-02-25 16:43:20 +0300

    create Env type

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import Env from '../types/Env';
 
 async function bootstrap() {
   require('dotenv').config();
+  const env: Env = process.env;
 
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.APP_PORT);
+  await app.listen(env.APP_PORT);
 }
 bootstrap();
---
+++ types/Env.ts
@@ -0,0 +1,6 @@
+interface Env extends NodeJS.ProcessEnv {
+  // @ts-ignore
+  APP_PORT?: number;
+}
+
+export default Env;

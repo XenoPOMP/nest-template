067b91ce532b430c40b3dac3d036190d9075058d
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-04-04 13:38:27 +0300

    edit MsSqlClient class

--- src/sql/MsSqlManager.ts
+++ src/sql/MsSqlManager.ts
@@ -1,6 +1,4 @@
-import SqlManager from './SqlManager';
-
-const sql = require('mssql');
+import * as sql from 'mssql';
 require('dotenv').config();
 const env = process.env;
 const clc = require('cli-color');
@@ -19,24 +17,65 @@ const config = {
   port: 38325,
 };
 
-class MsSqlManager extends SqlManager {
+class MsSqlManager {
   private messagePrefix: string = clc.blueBright('[MSSQL]');
 
+  private logTime(): string {
+    const date = new Date();
+
+    const refactorDate = (input: number): string => {
+      if (input < 10) {
+        return `0${input}`;
+      }
+
+      return `${input}`;
+    };
+
+    const day = (): string => refactorDate(date.getDate());
+    const month = (): string => refactorDate(date.getMonth() + 1);
+    const year = (): string => refactorDate(date.getFullYear());
+
+    const hours = (): string => refactorDate(date.getHours());
+    const minutes = (): string => refactorDate(date.getMinutes());
+    const seconds = (): string => refactorDate(date.getSeconds());
+
+    return clc.blueBright(
+      `[${day()}.${month()}.${year()}] [${hours()}:${minutes()}:${seconds()}]`,
+    );
+  }
+
   public async execQuery<QResult>(query: string): Promise<QResult> {
+    console.log(
+      '========================== Query start ==========================',
+    );
+
+    let pool;
+
     // prettier-ignore
     try {
-      let pool = await sql.connect(config);
-      console.log(`${this.messagePrefix} SQL Server connected...`);
+      pool = await sql.connect(config);
+      console.log(`${this.messagePrefix} ${this.logTime()} SQL Server connected...`);
 
       let res = await pool.request().query(query).then(res => {
-        console.log(`${this.messagePrefix} Request fulfilled successfully...`);
+        console.log(`${this.messagePrefix} ${this.logTime()} Request fulfilled successfully...`);
         return res;
       });
+
+      console.log(`${this.messagePrefix} ${this.logTime()} Find ${res?.recordsets[0].length} results.`);
+
+      console.log('=========================== Query end ===========================');
       return res.recordsets;
     }
     catch (error) {
-      console.log(`${this.messagePrefix} ${clc.red('mathus-error')}: ${error}`);
+      console.log(`${this.messagePrefix} ${this.logTime()} ${clc.red('mathus-error')}: ${error}`);
     }
+    finally {
+      pool.close();
+    }
+
+    console.log(
+      '========================== Query end ==========================',
+    );
   }
 }
 
--- src/sql/SqlManager.ts
+++
@@ -1,3 +0,0 @@
-class SqlManager {}
-
-export default SqlManager;

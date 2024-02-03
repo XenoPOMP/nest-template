48f58a442cdeeed6de659cd32471fa578ba1d5d8
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2024-02-03 18:52:14 +0300

    optimize code

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-	imports: [
-		// MysqlModule.forRootAsync({
-		// 	useFactory: () => ({
-		// 		host: process.env.MYSQL_HOST,
-		// 		database: process.env.MYSQL_DATABASE,
-		// 		user: process.env.MYSQL_USER,
-		// 		password: process.env.MYSQL_PASSWORD,
-		// 		port: parseInt(process.env.MYSQL_PORT),
-		// 	}),
-		// }),
-	],
+	imports: [],
 	controllers: [AppController],
 	providers: [AppService],
 })
